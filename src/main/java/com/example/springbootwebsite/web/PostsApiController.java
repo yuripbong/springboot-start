9e191bce1816f63dce5e86a9bb5a3e44e215953b
yuripbong <kyri200350@gmail.com>
2024-11-02 23:58:06 +0900

    fix: 컨트롤러 delete 메소드 사용

--- src/main/java/com/example/springbootwebsite/web/PostsApiController.java
+++ src/main/java/com/example/springbootwebsite/web/PostsApiController.java
@@ -30,4 +30,10 @@ public class PostsApiController {
     public PostsResponseDto findById(@PathVariable("id") Long id) {
         return postsService.findById(id);
     }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id) {
+        postsService.delete(id);
+        return id;
+    }
 }
