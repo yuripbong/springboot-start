cc8014eefe44bc74c4667e361ccf101988481348
yuripbong <kyri200350@gmail.com>
2024-11-02 23:50:13 +0900

    fix: update 기능 수정

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -4,6 +4,10 @@ var main = {
         $('#btn-save').on('click', function () {
             _this.save();
         });
+
+        $('#btn-update').on('click', function (){
+            _this.update();
+        });
     },
     save : function () {
         var data = {
@@ -47,3 +51,5 @@ var main = {
         })
     }
 };
+
+main.init();
