f9bc388a8302672da07989d685ea096c1d2a7fad
yuripbong <kyri200350@gmail.com>
2024-11-02 23:27:44 +0900

    fix: 수정 화면 연결 코드 추가

--- src/main/java/com/example/springbootwebsite/web/IndexController.java
+++ src/main/java/com/example/springbootwebsite/web/IndexController.java
@@ -1,9 +1,11 @@
 package com.example.springbootwebsite.web;
 
 import com.example.springbootwebsite.service.posts.PostsService;
+import com.example.springbootwebsite.web.dto.PostsResponseDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class IndexController {
@@ -23,5 +25,13 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("posts", dto);
+
+        return "posts-update";
     }
 }
