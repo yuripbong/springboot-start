0d7c0c2986c81b8c31f9f743311433147b5ec605
yuripbong <kyri200350@gmail.com>
2024-11-02 23:14:25 +0900

    fix: PostsApiController.java 주석 추가

--- src/main/java/com/example/springbootwebsite/web/PostsApiController.java
+++ src/main/java/com/example/springbootwebsite/web/PostsApiController.java
@@ -13,16 +13,19 @@ public class PostsApiController {
 
     private final PostsService postsService;
 
+    // 등록
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
     }
 
+    // 수정
     @PutMapping("/api/v1/posts/{id}")
     public Long update(@PathVariable("id") Long id, @RequestBody PostsUpdateRequestDto requestDto) {
         return postsService.update(id, requestDto);
     }
 
+    // 조횐
     @GetMapping("/api/v1/posts/{id}")
     public PostsResponseDto findById(@PathVariable("id") Long id) {
         return postsService.findById(id);
