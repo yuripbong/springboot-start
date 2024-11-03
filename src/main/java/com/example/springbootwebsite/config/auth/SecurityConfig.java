b6b29058d55e83faee82ccda8cf03ea33195c1bb
yuripbong <kyri200350@gmail.com>
2024-11-04 00:25:27 +0900

    feat: Spring Security 설정 추가 및 OAuth2 로그인 구성

---
+++ src/main/java/com/example/springbootwebsite/config/auth/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.example.springbootwebsite.config.auth;
+
+import com.example.springbootwebsite.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfiguration {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable().and()
+                .authorizeRequests()
+                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
+                .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+                .anyRequest().authenticated().and()
+                .logout().logoutSuccessUrl("/").and()
+                .oauth2Login().userInfoEndpoint().userService(customOAuth2UserService);
+    }
+}
