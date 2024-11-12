dfb3bd069f320b3910a5d5b5b030ce1d57bb16ef
yuripbong <kyri200350@gmail.com>
2024-11-12 23:14:21 +0900

    feat: 인증된 사용자 정보 선언

---
+++ src/main/java/com/example/springbootwebsite/config/auth/dto/SessionUser.java
@@ -0,0 +1,17 @@
+package com.example.springbootwebsite.config.auth.dto;
+
+import com.example.springbootwebsite.domain.user.User;
+import lombok.Getter;
+
+@Getter
+public class SessionUser {
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+}
