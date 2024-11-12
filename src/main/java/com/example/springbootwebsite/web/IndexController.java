cad1ea930ebfe369c8b0e63ebe1dbfd3b02fd14f
yuripbong <kyri200350@gmail.com>
2024-11-12 23:23:56 +0900

    feat: index.mustache userName 사용

--- src/main/java/com/example/springbootwebsite/web/IndexController.java
+++ src/main/java/com/example/springbootwebsite/web/IndexController.java
@@ -1,24 +1,31 @@
 package com.example.springbootwebsite.web;
 
+import com.example.springbootwebsite.config.auth.dto.SessionUser;
 import com.example.springbootwebsite.service.posts.PostsService;
 import com.example.springbootwebsite.web.dto.PostsResponseDto;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
-
-    public IndexController(PostsService postsService) {
-        this.postsService = postsService;
-    }
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
